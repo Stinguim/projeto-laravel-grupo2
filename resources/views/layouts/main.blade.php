0b6b96a986038680e0ed089b8ee4b03acf4777d6
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-14 10:15:47 -0100

    "Começo"

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/styles/main.css">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <div class="header_conteiner">
+
+        </div>
+    </header>
+    @yield('content')
+    <footer>
+        <div class="footer_container">
+
+        </div>
+    </footer>
+</body>
+</html>
