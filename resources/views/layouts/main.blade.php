66c5128c78140e71ec92e119845596dbbbaf7837
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-11-17 11:33:13 -0100

    #nav_bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,8 +8,17 @@
 </head>
 <body>
     <header>
-        <div class="header_conteiner">
-            <a></a>
+        <div class="header_container">
+            <div class="w_page_container">
+                <a><img src="" alt="app_logo"> Gestao Limpezas</a>
+            </div>
+            <nav class="nav_bar">
+                <a>Dashboard</a>
+                <a>Gestao</a>
+                <a>Limpezas</a>
+                <a>Calendario</a>
+                <a>Login</a>
+            </nav>
         </div>
     </header>
     @yield('content')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+    <body>
         @section('content')
-                <p>This is from the welcome page</p>
+            <p>This is from the welcome page</p>
         @endsection
     </body>
 </html>
