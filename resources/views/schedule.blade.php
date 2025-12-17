7599551987eb49c4d150092c660bda961e473e4f
Marcio-Lima123 <148306104+Marcio-Lima123@users.noreply.github.com>
2025-12-17 22:53:28 -0100

    #migration and supervisor cleaning requests

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -11,28 +11,60 @@
 <body>
     @section('content')
             <div class="s-container">
-                <h2>Cleaning To Do</h2>
+                @if(Auth::user()->user_type != 'supervisor')
+                    <h2>Cleaning To Do</h2>
+                @endif
+                @if(Auth::user()->user_type == 'supervisor')
+                    <h2>Cleaning Requests</h2>
+                @endif
                 <div class="sch-container">
                     <div class="cleanings-container">
-                        <table>
-                            <tr>
-                                <th>Date</th>
-                                <th>Accommodation</th>
-                                <th>Address</th>
-                                <th>State</th>
-                                <th>Team</th>
-                            </tr>
-                            @foreach($cleanings as $cleaning)
+                        @if(Auth::user()->user_type != 'supervisor')
+                            <table>
                                 <tr>
-                                    <td>{{$cleaning->date}}</td>
-                                    <td>{{$cleaning->lodging_name}}</td>
-                                    <td>{{$cleaning->address}}</td>
-                                    <td>{{$cleaning->state}}</td>
-                                    <td>{{$cleaning->team_id}}</td>
-                                    <td><a href="/schedule/{{$cleaning->lodging_id}}">View</a></td>
+                                    <th>Date</th>
+                                    <th>Accommodation</th>
+                                    <th>Address</th>
+                                    <th>State</th>
+                                    <th>Team</th>
                                 </tr>
-                            @endforeach
-                        </table>
+                                @foreach($cleanings as $cleaning)
+                                    <tr>
+                                        <td>{{$cleaning->date}}</td>
+                                        <td>{{$cleaning->lodging_name}}</td>
+                                        <td>{{$cleaning->address}}</td>
+                                        <td>{{$cleaning->state}}</td>
+                                        <td>{{$cleaning->team_id}}</td>
+                                        <td><a href="/schedule/{{$cleaning->lodging_id}}">View</a></td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @endif
+                        @if(Auth::user()->user_type == 'supervisor')
+                            <table>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Request Id</th>
+                                    <th>Lodging Id</th>
+                                    <th>Description</th>
+                                    <th>State</th>
+                                    <th>Team</th>
+                                    <th colspan="2">Request</th>
+                                </tr>
+                                @foreach($cleanings as $cleaning)
+                                    <tr>
+                                        <td>{{$cleaning->date}}</td>
+                                        <td>{{$cleaning->request_id}}</td>
+                                        <td>{{$cleaning->lodging_id}}</td>
+                                        <td>{{$cleaning->descricao}}</td>
+                                        <td>{{$cleaning->state}}</td>
+                                        <td>{{$cleaning->team_id}}</td>
+                                        <td><a href="/schedule/cleaning/accept/{{$cleaning->request_id}}">Accept</a></td>
+                                        <td><a href="/shcedule/cleaning/reject/{{$cleaning->request_id}}">Reject</a></td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @endif
                     </div>
                 </div>
             </div>
