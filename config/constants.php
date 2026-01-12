2b986ae0aaa96a8031a13d98db5c4a1770b2a919
SimaoCFerreira <2023109236@uac.pt>
2026-01-12 16:15:19 -0100

    Add constants.php into config directory

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'roles' => [
+        "admin",
+        "user",
+        "client",
+        "supervisor",
+        "employee"
+    ],
+    'permissions' => [
+        "admin" => false,
+        "user" => false,
+        "client" => false,
+        "supervisor" => false,
+        "employee" => false
+    ]
+];
