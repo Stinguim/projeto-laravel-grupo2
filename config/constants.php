de4f7836a46edcbb7cf955f231699d3c180fa58c
SimaoCFerreira <2023109236@uac.pt>
2026-01-13 16:25:44 -0100

    Remove user

--- config/constants.php
+++ config/constants.php
@@ -3,14 +3,12 @@
 return [
     'roles' => [
         "admin",
-        "user",
         "client",
         "supervisor",
         "employee"
     ],
     'permissions' => [
         "admin" => false,
-        "user" => false,
         "client" => false,
         "supervisor" => false,
         "employee" => false
